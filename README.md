524c26b560a763f455c40742c1e5ceab8d006a5d
cyclowns <notzombiedude@gmail.com>
2020-02-05 22:01:42 -0700

    INIT

---
+++ README.md
@@ -0,0 +1,14 @@
+# brs-edit
+
+A simple command-line application for Windows and Linux that allows you to interface and edit `.brs` (Brickadia Build Save) files.
+
+Using `brs-edit`, you can mass change colors, display info about build, edit multiple files at once, delete brick types, and more
+
+## Examples
+
+TODO
+
+## Installation
+
+TODO
+
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+// TODO brs-edit
+//      ! Get import working for brs crate
+//      ! Create a basic interpreter and REPL interface for editing
+//      ? command-line arguments interface as well
+// TODO REPL
+//      ! change name/descriptions
+//      % display info about save (# bricks, color count, ?mods)
+//      ! basic operations
+//      ! color -> another color 
+//      ? brick -> another brick? if possible but doubtful
+//      % randomization
+//      ! write directly to file/save as new
+//      ? load multiple files into buffer to edit all at once
+//      ? delete bricks
+// TODO release
+//      ! Binaries for windows/linux
+//      ! Get tests running
+//      ! Make examples of REPL commands
+//      ? CI/CD
+
+fn main() {
+    println!("Hello, world!");
+}
